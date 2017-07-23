64726b545233bd45bd2188041c90aff602f8f294
eXo5 <mike.yingling15@gmail.com>
2017-07-23 18:52:34 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// Include the Main React Dependencies
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Include the main Parent Component
+import Form from './components/Form';
+
+// This code here allows us to render our main component (in this case Parent)
+ReactDOM.render(<Form />, document.getElementById("root"));
